2b255451a9bab3bcf964098cebb2c36794f77c22
scienceopen <scienceopen@users.noreply.github.com>
2017-02-21 12:08:41 -0500

    doc

--- rfid2voice.py
+++ rfid2voice.py
@@ -1,47 +1,12 @@
-#!/usr/bin/env python3
-# Michael Hirsch 2014
+#!/usr/bin/env python
+from pyrfid import tagreader
 
-from pyserial import Serial
-from pandas import read_excel
-import pygame
-import numpy as np
-from scipy.io.wavfile import read
-
-def main(xls,rfidcode,sport):
-    
-    s = Serial(sport, baudrate=19200, timeout=1, bytesize=8,parity='N', stopbits=1, xonxoff=0, rtscts=0)
-    s.open()
-    bytesavail = s.inWaiting()
-    if bytesavail:
-        data = read_excel(xls,index_col=0)
-        wavfn = matchrfid(rfidcode,data)
-        playmatch(wavfn)
-    s.close()
-
-def matchrfid(tagnum,data):
-    return data.ix[tagnum,1]
-
-def doplot(raw):
-    from matplotlib.pyplot import figure,show
-    ax= figure().gca()
-    ax.plot(raw)
-    show()
-    
-def playmatch(wavfn):
-    fs,raw = read(wavfn)
-    raw = np.atleast_2d(raw)
-    r,c = raw.shape
-    if r<c: raw = raw.T
-    raw = raw[:20000,0] #mono, first 20000 samples
-    sample_rate = 44100
-    pygame.mixer.pre_init(sample_rate,size=-16,channels=1)
-    pygame.mixer.init()
-    sound = pygame.sndarray.make_sound((raw).astype(np.int16))
-    print('sound length' + str(sound.get_length()) + 'seconds')
-    sound.play(loops=0)
-    
-    doplot(raw)
-    
 if __name__ == '__main__':
-    from sys import argv
-    main('rfid2name.xlsx',int(argv[1]),'/dev/tty32')
+    from argparse import ArgumentParser
+    p = ArgumentParser()
+    p.add_argument('--dbfn',default='rfid2name.xlsx')
+    p.add_argument('-p','--port',help='serial port file ID',default='/dev/ttyUSB0')
+
+    p = p.parse_args()
+
+    tagreader(p.dbfn,p.port)
