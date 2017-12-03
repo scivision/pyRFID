94f7774202daa86a97e60fa6dd2504fe7a2e94f0
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-12-03 01:35:11 -0500

    template

--- setup.py
+++ setup.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python
-req = ['nose','pandas','scipy','pyserial']
-pipreq = ['pygame',]
-import pip
-try:
-    import conda.cli
-    conda.cli.main('install',*req)
-except Exception as e:    
-    pip.main(['install'] + req)
-pip.main(['install'] + pipreq)
-   
+install_requires = ['pandas','scipy','pyserial','pygame',]
+tests_require=['nose','coveralls']
 # %%
-from setuptools import setup #enables develop
+from setuptools import setup,find_packages
 
 setup(name='pyRFID',
-      packages=['pyrfid'],
+      packages=find_packages(),
 	  description='Example of reading RFID tag and playing sound with XLSX read',
 	  author='Michael Hirsch, Ph.D.',
 	  url='https://github.com/scivision/pyRFID',
 	  classifiers=[
-	        'Programming Language :: Python :: 3.6',
+	        'Programming Language :: Python :: 3',
 	        ],
+      python_requires='>=3.6',
+      install_requires=install_requires,
+      tests_require=tests_require,
+      extras_require={'tests':tests_require},
 	  )
